6841b89315b289fc82a710ced182c811c3e5bfc8
Amirsalar Heydari <amirsalaar@gmail.com>
2021-04-04 02:29:30 -0700

    Grab .env from ts root project

--- src/settings/globalSettings.ts
+++ src/settings/globalSettings.ts
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
 
+const baseDir = ["development", "test"].includes(process.env.NODE_ENV!)
+  ? path.join(__dirname, "../../../.env.")
+  : path.join(__dirname, "../../.env.");
+
 dotenv.config({
-  path: path.join(__dirname, "../../.env." + process.env.NODE_ENV),
+  path: path.join(baseDir + process.env.NODE_ENV),
   debug: process.env.NODE_ENV === "development" ? true : false,
 });
