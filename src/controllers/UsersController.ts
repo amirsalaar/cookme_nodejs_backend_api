faa343aa3d87388c697008bb36eeb2066385f4af
Amirsalar Heydari <amirsalaar@gmail.com>
2021-05-17 23:24:02 -0700

    Add auth as a middleware before getUserById method

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { UserService } from "../services/UserService";
 
 export class UsersController {
@@ -12,21 +12,14 @@ export class UsersController {
    * getUserById
    * @param req {Request}
    * @param res {Response}
-   * @param next {NextFunction}
    */
-  public getUserById = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<void> => {
+  public getUserById = async (req: Request, res: Response): Promise<void> => {
     try {
       const { id } = req.params;
       const result = await this.service.getUserById(parseInt(id));
 
       if (result === null) res.status(404).send();
       else res.status(200).json(result);
-
-      next();
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import auth from "../middlewares/Auth";
 import { UsersController } from "../controllers/UsersController";
 
 const userRoutes = Router();
 const userController = new UsersController();
 
-userRoutes.get("/users/:id", userController.getUserById);
+userRoutes.get("/users/:id", auth, userController.getUserById);
 
 export default userRoutes;
