32192ec75c15dbd978567083408c77f8bdc162ab
Amirsalar Heydari <amirsalaar@gmail.com>
2021-06-04 17:58:31 -0700

    Create user DTOs

---
+++ src/users/dtos/index.ts
@@ -0,0 +1,6 @@
+/**
+ * DTO: Data Transfer object for Creating a User
+ */
+export * from "./create.user.dto";
+export * from "./put.user.dto";
+export * from "./patch.user.dto";
---
+++ src/users/dtos/patch.user.dto.ts
@@ -0,0 +1,4 @@
+import { PutUserDto } from "./";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PatchUserDto extends Partial<PutUserDto> {}
