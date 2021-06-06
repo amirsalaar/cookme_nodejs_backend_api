a918fd0104a0e1df141159bdb8a90cac13920859
Amirsalar Heydari <amirsalaar@gmail.com>
2021-06-06 16:33:04 -0700

    Replace all users dao methods with a call to mongodb instance

--- src/users/daos/users.dao.ts
+++ src/users/daos/users.dao.ts
@@ -32,6 +32,75 @@ class UsersDao {
   constructor() {
     log("Created new instance of UsersDao");
   }
+
+  /**
+   * addUser
+   * @param {CreateUserDto} userFields
+   * @return {Promise<string>} userId
+   */
+  public async addUser(userFields: CreateUserDto): Promise<string> {
+    const userId = shortid.generate();
+    const user = new this.User({
+      _id: userId,
+      ...userFields,
+      permissionFlags: 1,
+    });
+    await user.save();
+    return userId;
+  }
+
+  /**
+   * getUserById
+   * @param {string} userId
+   */
+  public async getUserById(userId: string) {
+    return this.User.findOne({ _id: userId }).populate("User").exec();
+  }
+
+  /**
+   * getUsers
+   * @param {Number} limit
+   * @param {Number} page
+   */
+  public async getUsers(limit = 25, page = 0) {
+    return this.User.find()
+      .limit(limit)
+      .skip(limit * page)
+      .exec();
+  }
+
+  /**
+   * getUserByEmail
+   */
+  public async getUserByEmail(email: string) {
+    return this.User.findOne({ email }).exec();
+  }
+
+  /**
+   * updateUserById
+   * @param {string} userId
+   * @param {PatchUserDto | PutUserDto} userFields
+   */
+  public async updateUserById(
+    userId: string,
+    userFields: PatchUserDto | PutUserDto,
+  ) {
+    const existingUser = await this.User.findOneAndUpdate(
+      { _id: userId },
+      { $set: userFields },
+      { new: true },
+    ).exec();
+
+    return existingUser;
+  }
+
+  /**
+   * removeUserById
+   * @param {string} userId
+   */
+  public async removeUserById(userId: string) {
+    return this.User.deleteOne({ _id: userId }).exec();
+  }
 }
 
 export default new UsersDao(); // singleton class
