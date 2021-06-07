05353026204033c2524eb7b0cfe3e668c0bc23cc
Amirsalar Heydari <amirsalaar@gmail.com>
2021-06-06 22:44:47 -0700

    Implement refres-token endpoint

--- src/auth/auth.routes.config.ts
+++ src/auth/auth.routes.config.ts
@@ -4,6 +4,7 @@ import { CommonRoutesConfig } from "../common/common.routes.config";
 import authController from "./controllers/auth.controller";
 import authMiddleware from "./middleware/auth.middleware";
 import bodyValidationMiddleware from "../common/middleware/body.validation.middleware";
+import jwtMiddleware from "./middleware/jwt.middleware";
 
 export class AuthRoutes extends CommonRoutesConfig {
   constructor(app: Application, nameSpace: string | undefined = "/api/v2") {
@@ -16,6 +17,13 @@ export class AuthRoutes extends CommonRoutesConfig {
       body("password").isString(),
       bodyValidationMiddleware.verifyBodyFieldsErrors,
       authMiddleware.verifyUserPassword,
+      authController.createJWT,
+    ]);
+
+    this.app.post("/auth/refresh-token", [
+      jwtMiddleware.validJWTNeeded,
+      jwtMiddleware.verifyRefreshBodyField,
+      jwtMiddleware.validRefreshNeeded,
       authController.createJWT,
     ]);
     return this.app;
