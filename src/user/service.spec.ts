8a1899d862a17a981d06472e5d41bf6177510058
Amirsalar Heydari <amirsalaar@gmail.com>
2021-06-15 18:58:11 -0700

    Test getUsers

--- src/user/service.spec.ts
+++ src/user/service.spec.ts
@@ -2,20 +2,17 @@ import { UserRepository } from "./interface";
 import Service from "./service";
 
 describe("user service", () => {
-  let globalRepository: UserRepository;
+  const globalRepository: UserRepository = {
+    findUserById: jest.fn(),
+    addUser: jest.fn(),
+    getUsers: jest.fn(),
+    updateUserById: jest.fn(),
+    removeUserById: jest.fn(),
+    getUserByEmail: jest.fn(),
+    getUserByEmailWithPassword: jest.fn(),
+  };
 
   describe("getUser", () => {
-    beforeAll(() => {
-      globalRepository = {
-        findUserById: jest.fn(),
-        addUser: jest.fn(),
-        getUsers: jest.fn(),
-        updateUserById: jest.fn(),
-        removeUserById: jest.fn(),
-        getUserByEmail: jest.fn(),
-        getUserByEmailWithPassword: jest.fn(),
-      };
-    });
     it("gets the user from the database", async () => {
       const expected = {
         _id: "random_id_string",
@@ -37,6 +34,34 @@ describe("user service", () => {
       const result = await service.getById("random_id_string");
 
       expect(result).toMatchObject(expected);
+    });
+  });
+
+  describe("list users", () => {
+    it("returns the list of all users", async () => {
+      const expectedUsers = [...Array(10)].map((user, index) => {
+        return {
+          _id: `random_id_string ${index}`,
+          username: `random ${index}`,
+          email: `random_email ${index}`,
+          password: `random_pass ${index}`,
+          firstName: `random_fname ${index}`,
+          lastName: `rand_lname ${index}`,
+          permissionFlags: Math.ceil(Math.random() * 10),
+          createdAt: Date.now(),
+        };
+      });
+
+      const repository = {
+        ...globalRepository,
+        getUsers: jest.fn().mockResolvedValueOnce(expectedUsers),
+      };
+
+      const service = Service(repository);
+      const result = await service.list(10, 0);
+
+      expect(result.length).toEqual(10);
+      expect(result).toMatchObject(expectedUsers);
     });
   });
 });
